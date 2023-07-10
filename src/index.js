aff6dcc8cb49c6b94d6e8f78262be931f0fc641a
Shana Matthews <shana.l.matthews@gmail.com>
2023-07-10 13:54:00 -0700

    Added an error and some other stuff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-// Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://14b7cf933a9547e4a57b63c097e596e8@o4505449964175360.ingest.sentry.io/4505506997403648",
+  integrations: [new Sentry.BrowserTracing({}), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
